dc06de9ef144806cb6edf4df0c7445ea6ea1f851
jdrake16 <stovetops111@gmail.com>
2020-07-01 19:30:15 -0600

    added java compile options.

--- app/src/main/java/edu/cnm/deepdive/quotes/service/QuotesDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/quotes/service/QuotesDatabase.java
@@ -6,18 +6,22 @@ import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverter;
+import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 import edu.cnm.deepdive.quotes.R;
 import edu.cnm.deepdive.quotes.model.dao.QuoteDao;
 import edu.cnm.deepdive.quotes.model.dao.SourceDao;
 import edu.cnm.deepdive.quotes.model.entity.Quote;
 import edu.cnm.deepdive.quotes.model.entity.Source;
+import edu.cnm.deepdive.quotes.service.QuotesDatabase.Converters;
 import io.reactivex.schedulers.Schedulers;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -32,9 +36,10 @@ import org.apache.commons.csv.CSVRecord;
     version = 1,
     exportSchema = true
 )
+@TypeConverters({Converters.class})
 public abstract class QuotesDatabase extends RoomDatabase {
 
-  private static final  String DB_NAME = "quotes_db";
+  private static final String DB_NAME = "quotes_db";
 
   private static Application context;
 
@@ -78,20 +83,20 @@ public abstract class QuotesDatabase extends RoomDatabase {
           CSVParser parser = CSVParser.parse(
               reader, CSVFormat.EXCEL.withIgnoreSurroundingSpaces().withIgnoreEmptyLines());
       ) {
-       Map<Source, List<Quote>> map = new HashMap<>();
-       for (CSVRecord record : parser) {
-         Source source = null;
-         String sourceName = record.get(0).trim();
-         if (!sourceName.isEmpty()) {
-           source = new Source();
-           source.setName(sourceName);
-         }
-         List<Quote> quotes = map.computeIfAbsent(source, (s) -> new LinkedList<>());
-         Quote quote = new Quote();
-         quote.setText(record.get(1).trim());
-         quotes.add(quote);
-       }
-       return map;
+        Map<Source, List<Quote>> map = new HashMap<>();
+        for (CSVRecord record : parser) {
+          Source source = null;
+          String sourceName = record.get(0).trim();
+          if (!sourceName.isEmpty()) {
+            source = new Source();
+            source.setName(sourceName);
+          }
+          List<Quote> quotes = map.computeIfAbsent(source, (s) -> new LinkedList<>());
+          Quote quote = new Quote();
+          quote.setText(record.get(1).trim());
+          quotes.add(quote);
+        }
+        return map;
       }
     }
 
@@ -120,9 +125,26 @@ public abstract class QuotesDatabase extends RoomDatabase {
             return quoteDao.insert(quotes);
           })
           .subscribe(
-              (quoteIds) -> {},
-              (throwable) -> {throw new RuntimeException(throwable);}
+              (quoteIds) -> {
+              },
+              (throwable) -> {
+                throw new RuntimeException(throwable);
+              }
           );
+    }
+  }
+
+
+  public static class Converters {
+
+    @TypeConverter
+    public static Long dateToLong(Date value) {
+      return (value != null) ? value.getTime() : null;
+    }
+
+    @TypeConverter
+    public static Date longToDate(Long value) {
+      return (value != null) ? new Date(value) : null;
     }
   }
 
